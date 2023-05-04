bd2e74a2dd1022200d4d3a035d8d963dd28a6444
Replit user <>
2023-05-04 00:05:35 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -81,8 +81,10 @@ int is_valid(Node* n){
     for(p = 0 ; p < 9 ; p++){
       int i = 3*(k/3) + (p/3);
       int j = 3*(k%3) + (p%3);
-      if(n -> sudo[i][j] != 0 && marca[n -> sudo[i][j]] == 1){
-        return 0;
+      if(n -> sudo[i][j] != 0){ 
+        if(marca[n -> sudo[i][j]] == 1){
+          return 0;
+        }
       }
       marca[n -> sudo[i][j]] = 1;
     }
