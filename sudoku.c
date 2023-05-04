b76863107283b5b080e267787a26209c4cfabbd0
Replit user <>
2023-05-04 20:13:17 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -114,19 +114,20 @@ List* get_adj_nodes(Node* n){
   return list;
 }
 
-
 int is_final(Node* n){
   int i,j;
   for(i = 0 ; i < 9 ; i++){
     for(j = 0 ; j < 9 ; j++){
-      if(n -> sudo[i][j] == 0)
+      if(n -> sudo[i][j] == 0){
         return 0;
+      }
     }
   }
   return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
+  Stack* s = createStack();
   return NULL;
 }
 
